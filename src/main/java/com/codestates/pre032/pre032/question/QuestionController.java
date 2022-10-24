186e864828fd86349aa181854b8ec5c06a54e5c9
woong-sung <cjswo138@gmail.com>
2022-10-24 23:27:41 +0900

    feat: controller(updateQuestion), service(update, find) 구현

--- src/main/java/com/codestates/pre032/pre032/question/QuestionController.java
+++ src/main/java/com/codestates/pre032/pre032/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.codestates.pre032.pre032.question;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/questions")
@@ -19,7 +17,7 @@ public class QuestionController {
         this.questionMapper = questionMapper;
     }
 
-    @PostMapping("/create/{id}")
+    @PostMapping("/create")
     public ResponseEntity createQuestion(@RequestBody QuestionDto.Post requestBody){
         Question question = questionService.create(questionMapper.questionPostDtoToQuestion(requestBody));
 
@@ -28,4 +26,22 @@ public class QuestionController {
         return new ResponseEntity<> (response, HttpStatus.CREATED);
     }
 
+    @PatchMapping("/update/{id}")
+    public ResponseEntity updateQuestion(@PathVariable("id") Long id,
+                                         @RequestBody QuestionDto.Patch requestBody){
+        Question question = questionService.update(id, questionMapper.questionPatchDtoToQuestion(requestBody));
+
+        QuestionDto.response response = questionMapper.questionToQuestionResponseDto(question);
+
+        return new ResponseEntity<> (response, HttpStatus.OK);
+    }
+
+    @GetMapping("/create/{id}")
+    public ResponseEntity questionDetail(@PathVariable("id") Long id){
+        Question question = questionService.find(id);
+
+        QuestionDto.response response = questionMapper.questionToQuestionResponseDto(question);
+
+        return new ResponseEntity<> (response, HttpStatus.OK);
+    }
 }
--- src/main/java/com/codestates/pre032/pre032/question/QuestionService.java
+++ src/main/java/com/codestates/pre032/pre032/question/QuestionService.java
@@ -1,8 +1,10 @@
 package com.codestates.pre032.pre032.question;
 
+import com.codestates.pre032.pre032.exception.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -23,5 +25,27 @@ public class QuestionService {
         this.questionRepository.save(newQuestion);
 
         return question;
+    }
+
+    public Question update(Long id, Question question) {
+        Question updateQuestion = find(id);
+
+        updateQuestion.setSubject(question.getSubject());
+        updateQuestion.setContent(question.getContent());
+        updateQuestion.setCreatedAt(LocalDateTime.now());
+        updateQuestion.setModifiedAt(LocalDateTime.now());
+
+        this.questionRepository.save(updateQuestion);
+
+        return question;
+    }
+
+    public Question find(Long id) {
+        Optional<Question> findQuestion = this.questionRepository.findById(id);
+        if (findQuestion.isPresent()) {
+            return findQuestion.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }

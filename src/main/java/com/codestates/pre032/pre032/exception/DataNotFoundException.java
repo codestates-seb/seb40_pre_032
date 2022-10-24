121695dabc400f8942fa91a74d8a3613f0683ae7
woong-sung <cjswo138@gmail.com>
2022-10-24 23:28:19 +0900

    feat: exception 패키지 생성, DataNotFoundException 생성

---
+++ src/main/java/com/codestates/pre032/pre032/exception/DataNotFoundException.java
@@ -0,0 +1,13 @@
+package com.codestates.pre032.pre032.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "entity not found")
+public class DataNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}

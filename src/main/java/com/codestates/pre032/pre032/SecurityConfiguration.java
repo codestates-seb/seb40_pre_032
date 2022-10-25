c19c4d644184d4b1259fc34d9fbf7a92f52a7e74
woong-sung <cjswo138@gmail.com>
2022-10-25 14:09:33 +0900

    test: postman 테스트를 위한 임시 시큐리티

---
+++ src/main/java/com/codestates/pre032/pre032/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.codestates.pre032.pre032;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors(withDefaults())    // (3)
+                .formLogin().disable()   // (4)
+                .httpBasic().disable()   // (5)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()                // (6)
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

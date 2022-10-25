d05a792675fcdf6d8de33b6efd4d5c8610d00197
woong-sung <cjswo138@gmail.com>
2022-10-26 00:59:24 +0900

    feat: tag 기능 추가

--- src/main/java/com/codestates/pre032/pre032/tag/QuestionTag.java
+++ src/main/java/com/codestates/pre032/pre032/tag/QuestionTag.java
@@ -2,6 +2,7 @@ package com.codestates.pre032.pre032.tag;
 
 import com.codestates.pre032.pre032.question.Question;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,21 +10,26 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class QuestionTag {
     public QuestionTag(Question question, String tag) {
         this.question = question;
         this.tag = new Tag(tag);
     }
 
+    public QuestionTag(Question question) {
+        this.question = question;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long QuestionTagId;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "QUESTION_ID")
     private Question question;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "TAG_ID")
     private Tag tag;
 

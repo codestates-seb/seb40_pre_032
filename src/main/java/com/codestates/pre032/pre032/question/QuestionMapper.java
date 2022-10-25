1c0e7c9aa11755af065f1c82335741b0593f9870
woong-sung <cjswo138@gmail.com>
2022-10-25 15:51:35 +0900

    feat: 질문목록을 responseDto로 변환기능 추가

--- src/main/java/com/codestates/pre032/pre032/question/QuestionMapper.java
+++ src/main/java/com/codestates/pre032/pre032/question/QuestionMapper.java
@@ -2,6 +2,8 @@ package com.codestates.pre032.pre032.question;
 
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
     Question questionPostDtoToQuestion(QuestionDto.Post questionPostDto);
@@ -9,4 +11,6 @@ public interface QuestionMapper {
     Question questionPatchDtoToQuestion(QuestionDto.Patch questionPatchDto);
 
     QuestionDto.response questionToQuestionResponseDto(Question question);
+
+    List<QuestionDto.response> questionsToQuestionResponseDto(List<Question> questions);
 }

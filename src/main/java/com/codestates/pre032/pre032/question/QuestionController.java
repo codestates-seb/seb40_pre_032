74bf686b7125d835983503ebac963a2d8f25c678
woong-sung <cjswo138@gmail.com>
2022-10-25 15:50:00 +0900

    feat: 삭제 기능, 전체 질문 조회 기능 추가

--- src/main/java/com/codestates/pre032/pre032/question/QuestionController.java
+++ src/main/java/com/codestates/pre032/pre032/question/QuestionController.java
@@ -1,9 +1,14 @@
 package com.codestates.pre032.pre032.question;
 
+import com.codestates.pre032.pre032.dto.MultiResponseDto;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/questions")
@@ -17,8 +22,9 @@ public class QuestionController {
         this.questionMapper = questionMapper;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity createQuestion(@RequestBody QuestionDto.Post requestBody){
+    // 작성 기능
+    @PostMapping("/ask")
+    public ResponseEntity askQuestion(@Validated @RequestBody QuestionDto.Post requestBody){
         Question question = questionService.create(questionMapper.questionPostDtoToQuestion(requestBody));
 
         QuestionDto.response response = questionMapper.questionToQuestionResponseDto(question);
@@ -26,6 +32,19 @@ public class QuestionController {
         return new ResponseEntity<> (response, HttpStatus.CREATED);
     }
 
+//    @GetMapping("/ask")
+//    public String askForm(){
+//        return "askForm 주소";
+//    }
+
+    @GetMapping("/update/{id}")
+    public ResponseEntity updateForm(@PathVariable("id") Long id){
+        Question question = questionService.find(id);
+        QuestionDto.response response = questionMapper.questionToQuestionResponseDto(question);
+        return new ResponseEntity<> (response, HttpStatus.OK);
+    }
+
+    // 수정 기능
     @PatchMapping("/update/{id}")
     public ResponseEntity updateQuestion(@PathVariable("id") Long id,
                                          @RequestBody QuestionDto.Patch requestBody){
@@ -36,12 +55,38 @@ public class QuestionController {
         return new ResponseEntity<> (response, HttpStatus.OK);
     }
 
-    @GetMapping("/create/{id}")
+    // 한 질문 자세히 보기
+    @GetMapping("/{id}")
     public ResponseEntity questionDetail(@PathVariable("id") Long id){
         Question question = questionService.find(id);
 
         QuestionDto.response response = questionMapper.questionToQuestionResponseDto(question);
 
         return new ResponseEntity<> (response, HttpStatus.OK);
+    }
+
+    // 전체 질문 조회하기
+    @GetMapping("/")
+    public ResponseEntity allQuestion(@RequestParam(value = "page", defaultValue = "0") int page){
+        Page<Question> paging = this.questionService.getQuestions(page);
+        List<Question> questions = paging.getContent();
+
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(questionMapper.questionsToQuestionResponseDto(questions), paging), HttpStatus.OK);
+    }
+
+    // 삭제 기능
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteQuestion(@PathVariable Long id){
+        this.questionService.delete(id);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    // 테스트용 메서드 모든 질문 삭제
+    @PostMapping("/deleteAll")
+    public ResponseEntity deleteAll(){
+        questionService.deleteAll();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

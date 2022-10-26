4d1a9ba21d5fd171c768e88775e92ad89c298337
leejehoon92 <2eejehoon@gmail.com>
2022-10-26 21:22:22 +0900

    style: 로고 추가

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<footer className="h-80 text-gray-300 justify-evenly w-full bg-zinc-800 p-8">
-			<nav className="text-sm flex flex-row justify-center font-bold">
+		<footer className="h-80 text-gray-300 flex flex-row justify-center w-full bg-zinc-800 p-8">
+			<div className="w-10 right-0">
+				<svg
+					aria-hidden="true"
+					className=""
+					width="32"
+					height="37"
+					viewBox="0 0 32 37"
+				>
+					<path d="M26 33v-9h4v13H0V24h4v9h22Z" fill="#BCBBBB" />
+					<path
+						d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z"
+						fill="#F48024"
+					/>
+				</svg>
+			</div>
+			<nav className="text-sm flex flex-row justify-between font-bold">
 				<div className="w-52">
 					<h4 className="mb-4">STACK OVERFLOW</h4>
 					<ul className="font-normal text-gray-500">

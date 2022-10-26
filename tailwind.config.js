7fafaf98ce8912a4c2b59b864614e97a6a8ba861
dohyeons <ddhhss0603@gmail.com>
2022-10-26 13:54:54 +0900

    fix: 테일윈드 파일 콘플릭트 해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
@@ -13,13 +12,6 @@ module.exports = {
 			little: '3px',
 		},
 	},
-=======
-	// 템플릿 파일의 경로 설정 👀
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
 	variants: {},
->>>>>>> 1208ff84db666c9e47bd80ffef69aa61b1f30965
 	plugins: [],
 };

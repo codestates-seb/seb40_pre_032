c6a33d0285c7808187d76ec996380638fade7338
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-10-26 16:36:59 +0900

    style: css 수정

--- src/component/Editor.js
+++ src/component/Editor.js
@@ -32,12 +32,12 @@ export default function App() {
 				theme="snow"
 				value={editorContent}
 				onChange={onEditorStateChange}
-				className="h-[600px] px-5 mt-5 shadow-gray-700 rounded-xl"
+				className="h-[600px] px-5 mt-5 shadow-gray-700"
 			/>
 			<p className="px-10">{errors.content && '입력해주세요'}</p>
 
 			<button
-				className="absolute bottom-10 left-10 rounded-lg h-10 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
+				className="absolute bottom-10 left-10 h-10 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
 				type="submit"
 				value="Review Your Question"
 				onClick={handleSubmit(onSubmit)}

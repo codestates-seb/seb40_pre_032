fc3fc57c0c3a3f364458680ad97a63b06f8fd78b
dohyeons <ddhhss0603@gmail.com>
2022-10-26 21:36:11 +0900

    fix: tailwind config파일 little삭제

--- src/component/ButtonGroup.js
+++ src/component/ButtonGroup.js
@@ -4,11 +4,11 @@ import { IoFilterSharp } from 'react-icons/io5';
 function ButtonGroup() {
 	return (
 		<div className="w-[465px] flex">
-			<div className="flex rounded-little h-[34px] items-center mr-4">
+			<div className="flex rounded  h-[34px] items-center mr-4">
 				<a
 					href="https://stackoverflow.com/questions"
 					aria-current="page"
-					className="py-[5px] px-4 text-[13px] h-full  text-[#64686e] bg-white rounded-l-little border border-[hsl(210,8%,65%)] hover:bg-gray-100  hover:text-gray-900 focus:z-10   focus:text-[#64686e]  "
+					className="rounded-l py-[5px] px-4 text-[13px]  h-full text-[#64686e] bg-white border border-[hsl(210,8%,65%)] hover:bg-gray-100  hover:text-gray-900 focus:z-10   focus:text-[#64686e]  "
 				>
 					<div>Newest</div>
 				</a>
@@ -20,12 +20,12 @@ function ButtonGroup() {
 				</a>
 				<a
 					href="https://stackoverflow.com/questions"
-					className="py-[5px] px-4 text-[13px] h-full text-[#64686e] bg-white rounded-r-little border border-[hsl(210,8%,65%)] hover:bg-gray-100 hover:text-gray-900 focus:z-10   focus:text-[#64686e]  "
+					className="rounded-r py-[5px] px-4 text-[13px] h-full text-[#64686e] bg-white border border-[hsl(210,8%,65%)] hover:bg-gray-100 hover:text-gray-900 focus:z-10   focus:text-[#64686e]  "
 				>
 					<div>Unanswered</div>
 				</a>
 			</div>
-			<div className="border-solid flex border-border-blue border h-[34px] hover:bg-[#b3d3e9] rounded-little text-[#37739c] bg-[#e0ecf3] text-[13px]">
+			<div className="rounded border-solid flex border-border-blue border h-[34px] hover:bg-[#b3d3e9] text-[#37739c] bg-[#e0ecf3] text-[13px]">
 				<button
 					type="button"
 					className="w-[68px] flex items-center justify-center decoration-[#e0ecf3] "

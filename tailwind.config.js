6ac9ab62f61a0bba2106b7a3dbefa022deafaa71
dohyeons <ddhhss0603@gmail.com>
2022-10-27 00:06:27 +0900

    docs: tailwind.config와 app.js 수정본으로 교체

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
 				'border-blue': 'hsl(205,41%,63%)',
 				'button-blue': '#e0ecf3',
 			},
+			boxShadow: {
+				'1top':
+					'0 1px 2px hsla(0,0%,0%,0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05)',
+				sidebar:
+					'0 1px 2px hsla(0,0%,0%,0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05)',
+			},
 		},
 		borderRadius: {
 			little: '3px',

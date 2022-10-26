2877eb8652a1edbf67828810e2bb186998182881
dohyeons <ddhhss0603@gmail.com>
2022-10-26 09:16:57 +0900

    style: 인풋창 구현 및 폰트사이즈 조정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 export default function App() {
 	return (
-		<header className="h-[47px] bg-[#f7faf9] flex flex-row items-center shadow-1top">
+		<header className="h-[47px] bg-[#f7faf9] flex flex-row items-center shadow-1top text-[13px]">
 			<div className="w-[1264px] h-full flex items-center mx-auto my-0 max-w-full">
 				<a
 					href="https://stackoverflow.com/"
@@ -11,16 +11,19 @@ export default function App() {
 				>
 					<img
 						alt="stackoverflow logo"
-						src="src/img/stack.png "
+						src="/img/stack.png "
 						className="w-[150px] h-[30px]"
 					/>
 				</a>
 				<div className="w-[82px] h-[33px] cursor-pointer flex items-center justify-center rounded-large hover:bg-[hsl(210,8%,90%)] whitespace-nowrap">
 					<span>Product</span>
 				</div>
-				<form className="align-baseline text-[100%] px-2">
-					<input className="cursor-text" />
-					<HiMagnifyingGlass />
+				<form className="align-baseline relative text-[100%] flex px-2 w-[773px]">
+					<HiMagnifyingGlass className="absolute left-[15px] mt-[9px]" />
+					<input
+						placeholder="Search..."
+						className="cursor-text border border-[#babec4] focus:ring-[5px] focus:ring-offset focus:ring-blue-500/[.10]  focus:outline focus:border-[0.5px] focus:outline-[hsl(206,90%,69.5%)] rounded-[3px] border-solid truncate w-full pl-[30px] h-[33px] focus: "
+					/>
 				</form>
 			</div>
 		</header>

f869e3903fb6825255b598455179616e99ebaab5
dohyeons <ddhhss0603@gmail.com>
2022-10-26 22:21:08 +0900

    style: tailwind.config.js 에 shadow 커스텀 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,9 @@ module.exports = {
 			boxShadow: {
 				'1top':
 					'0 1px 2px hsla(0,0%,0%,0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05)',
+				sidebar:
+					'0 1px 2px hsla(0,0%,0%,0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05)',
 			},
-		},
-		borderRadius: {
-			large: '1000px',
 		},
 	},
 	plugins: [],

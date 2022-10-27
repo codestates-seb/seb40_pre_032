e3ac5d549000069efde3363cee0e53d87af839ca
dohyeons <ddhhss0603@gmail.com>
2022-10-27 13:13:04 +0900

    style: app.js에 Sidebar 다시 삽입

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './component/Sidebar';
 import NotFound from './component/NotFound';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="questions" element={<Sidebar />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>

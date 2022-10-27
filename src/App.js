107ef755c18be777d5e52a8b09e57dd58b5e5fb0
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-10-27 18:19:57 +0900

    style:answer 컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import LoginHedaer from './components/LoginHedaer';
+import Answer from './components/Answer/Answer';
+// import LoginHeader from './components/LoginHeader';
 
 export default function App() {
 	return (
 		<div>
-			<LoginHedaer />
+			<Answer />
 		</div>
 	);
 }

f35a2ca9702c363392bc93428ab8ecbc6938bbc5
leejehoon92 <2eejehoon@gmail.com>
2022-10-29 00:38:51 +0900

    feat: axios 설치

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
 import './index.css';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider clinet={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );

cb9388419c8aaea84c58129b2804c1e658aee36f
leejehoon92 <106265483+leejehoon92@users.noreply.github.com>
2022-10-28 11:19:51 +0900

    Revert "Revert "Revert "[#42] 답변 컴포넌트 제작"""

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	singleQuote: true, // ' '(작은 따옴표) 사용
-	semi: true, // 코드의 끝에 세미콜론 사용
-	useTabs: true, // 들여쓰기 사용
-	tabWidth: 2, // 들여쓰기 공백 수
-	trailingComma: 'all', // 모든 문법 끝에 ,(콤마) 사용
-	printWidth: 80, // 한줄 최대 글자수
-	arrowParens: 'always', // 화살표함수 () => 괄호 항상 사용
-};

559f7baa6c41d165fb8823157dd7e8265816f4d3
leejehoon92 <106265483+leejehoon92@users.noreply.github.com>
2022-10-28 11:14:02 +0900

    Revert "Revert "[#42] 답변 컴포넌트 제작""

---
+++ src/components/Answer/Arrow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Arrow() {
+	return (
+		<>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6 hover:text-red-500 cursor-pointer"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M4.5 15.75l7.5-7.5 7.5 7.5"
+				/>
+			</svg>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				strokeWidth={1.5}
+				stroke="currentColor"
+				className="w-6 h-6 hover:text-red-500 cursor-pointer"
+			>
+				<path
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+				/>
+			</svg>
+		</>
+	);
+}
+
+export default Arrow;

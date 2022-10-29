3a7f713a1bfbf392d0f39772a491439383fc542b
leejehoon92 <2eejehoon@gmail.com>
2022-10-29 12:13:34 +0900

    feat: react-query 초기세팅 및 Devtools 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import NotFound from './components/NotFound';
 import Login from './routes/login';
 import Logout from './routes/logout';
@@ -7,17 +9,22 @@ import SignUp from './routes/signUp';
 import MyPage from './routes/myPage';
 import QuestionDetail from './routes/QuestionDetail';
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="mypage" element={<MyPage />} />
-				<Route path="login" element={<Login />} />
-				<Route path="logout" element={<Logout />} />
-				<Route path="signup" element={<SignUp />} />
-				<Route path="questions/:questionId" element={<QuestionDetail />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="mypage" element={<MyPage />} />
+					<Route path="login" element={<Login />} />
+					<Route path="logout" element={<Logout />} />
+					<Route path="signup" element={<SignUp />} />
+					<Route path="questions/:questionId" element={<QuestionDetail />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }

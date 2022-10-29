b4b2d24331369c67bc9ca857a5c09b332a81cb65
leejehoon92 <2eejehoon@gmail.com>
2022-10-29 11:20:08 +0900

    fix:index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import './index.css';
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-			<ReactQueryDevtools />
-		</QueryClientProvider>
-		,
+		<App />,
 	</React.StrictMode>,
 );

ca3ffdd377efaa3c7f0259e58753c9fafc502ee3
leejehoon92 <2eejehoon@gmail.com>
2022-10-30 14:40:32 +0900

    [#57] feat: 상세페이지 질문 및 답변 조회 기능 구현

--- src/components/QuestionDetail/QuestionUserInfo.js
+++ src/components/QuestionDetail/QuestionUserInfo.js
@@ -1,13 +1,42 @@
 import React from 'react';
-// import axios from 'axios';
-// import { useParams } from 'react-router-dom';
-// import { useQuery } from 'react-query';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
 
 function QuestionUserInfo() {
-	// const { id } = useParams();
-	// const { data } = useQuery(['question', id], () => {
-	// 	return axios.get(`http://localhost:4000/questions/${id}`);
-	// });
+	const { id } = useParams();
+	const { data } = useQuery(['question', id], () => {
+		return axios.get(`http://localhost:4000/questions/${id}`);
+	});
+
+	function elapsed(string) {
+		const today = new Date();
+		const targetDate = new Date(string);
+		const elapsedTime = today.getDate() - targetDate.getDate();
+
+		if (elapsedTime === 0) {
+			return 'today';
+		}
+		if (elapsedTime <= 30) {
+			if (elapsedTime === 1) {
+				return '1 day ago';
+			}
+			return `${elapsedTime} days ago`;
+		}
+		if (elapsedTime > 30 && elapsedTime < 365) {
+			if (Math.round(elapsedTime / 30) === 1) {
+				return '1 month ago';
+			}
+			return `${Math.round(elapsedTime / 30)} months ago`;
+		}
+		if (elapsedTime > 365) {
+			if (Math.round(elapsedTime / 365) === 1) {
+				return '1 year ago';
+			}
+			return `${Math.round(elapsedTime / 365)} years ago`;
+		}
+		return null;
+	}
 
 	return (
 		<div className="flex flex-row h-[50px]">
@@ -29,8 +58,10 @@ function QuestionUserInfo() {
 				</button>
 			</div>
 			<div className="w-[190px] bg-sky-100 px-2 py-1">
-				<div className="text-gray-500 text-sm">asked (createdAt) ago</div>
-				<div className="text-blue-500 text-sm">(author)</div>
+				<div className="text-gray-500 text-sm">
+					asked {elapsed(data?.data.creationDate)}
+				</div>
+				<div className="text-blue-500 text-sm">{data?.data.displayName}</div>
 			</div>
 		</div>
 	);

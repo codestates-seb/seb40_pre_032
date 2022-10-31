289472c0665675c6f88bc9046dd7dd9c6c5af763
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-10-31 15:19:21 +0900

    feat: react-hook-form validation 적용

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -13,7 +13,7 @@ function Editor() {
 		setValue,
 		watch,
 		formState: { errors },
-	} = useForm();
+	} = useForm({ mode: 'onChange' });
 
 	useEffect(() => {
 		register('content', { required: true, minLength: 11 });
@@ -34,16 +34,19 @@ function Editor() {
 		<main className="flex items-center mx-auto">
 			<article>
 				<h1 className="font-bold ml-12 mt-4 text-2xl">Ask a Question</h1>
-				<section className="shadow w-[1000px] h-[800px] px-2 mx-10 mt-10 border border-gray-600 bg-white">
+				<section className=" shadow w-[1000px] h-[800px] px-2 mx-10 mt-10 border border-gray-600 bg-white">
 					<h3 className="font-semibold">Title</h3>
 					<div className="text-gray-700 mb-2">
 						Be specific and imagine you’re asking a question to another person
 					</div>
 					<input
+						/* eslint-disable react/jsx-props-no-spreading */
+						{...register('title', { required: true })}
 						type="text"
 						className="w-full border-2 px-2 py-1 leading-loose"
 						placeholder="e.g Is there an R function for findiong the index of an element in vector?"
 					/>
+					{errors.title?.message}
 					<h3 className="font-semibold mt-2">Body</h3>
 					<span className="text-gray-700">
 						Include all the information someone would need to answer your
@@ -51,15 +54,16 @@ function Editor() {
 					</span>
 					<form onSubmit={handleSubmit(onSubmit)}>
 						<ReactQuill
+							{...register('content', { required: true })}
 							theme="snow"
 							value={editorContent}
 							onChange={onEditorStateChange}
-							className="h-96 px-2 mt-3 relative right-2 shadow-gray-700 "
+							className="h-96 px-2 mt-3 relative right-2 shadow-gray-700"
 						/>
-						<p className="px-10">{errors.content && '입력해주세요'}</p>
+						<p className="px-10">{errors.content?.message && '입력해주세요'}</p>
 						<TagInput />
 						<button
-							className="fixed text-white bottom-14 left-10 h-10 mb-10 ml-6 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
+							className="text-white h-10 mt-10 ml-3 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
 							type="submit"
 							value="Review Your Question"
 							onClick={handleSubmit(onSubmit)}

5eea722a7870a4ff88cc952e0d46c9cc4fdda4ff
dohyeons <ddhhss0603@gmail.com>
2022-10-31 16:11:23 +0900

    쿼리 및 액시오스 설치

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import MainPage from './components/MainPage/MainPage';
 import Login from './routes/login';
 import SignUp from './routes/signUp';
 import Editor from './components/Editor';
 
+const queryClient = new QueryClient();
+
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/ask" element={<Editor />} />
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/ask" element={<Editor />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	);
 }

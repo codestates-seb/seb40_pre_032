df869f8d1cb46cfafc15da55a33da74431147196
dohyeons <ddhhss0603@gmail.com>
2022-10-31 21:33:14 +0900

    feat: react query를 통해 서버로부터 데이터 응답 받음

--- src/components/Qlist.js
+++ src/components/Qlist.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 import items from './Dummydata';
 import QuestionHeading from './MainPage/QuestionHeading';
 import NumNBtn from './MainPage/NumNBtn';
@@ -9,17 +11,20 @@ function Qlist() {
 	const [limit, setLimit] = useState(15);
 	const [page, setPage] = useState(1);
 	const offset = (page - 1) * limit;
-	// const questions = items;
 	const [questions, setQuestions] = useState([]);
 
 	useEffect(() => {
 		setQuestions(items);
 	}, []);
+	const { data } = useQuery(['questions'], () => {
+		return axios.get('http://localhost:4000/items');
+	});
+	console.log(data?.data);
 	return (
 		<div className="p-[24px] w-[727px]  border-l-[1px] border-solid border-[hsl(210,8%,85%)] mb-4">
 			<QuestionHeading />
-			<NumNBtn questions={questions} />
-			<AllQuestions questions={questions} offset={offset} limit={limit} />
+			<NumNBtn questions={data?.data} />
+			<AllQuestions questions={data?.data} offset={offset} limit={limit} />
 			<Pagination
 				limit={limit}
 				questions={questions}

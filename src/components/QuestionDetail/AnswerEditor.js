1d4985e2b8f2a611a96eaaa21c781e866d51c60f
leejehoon92 <2eejehoon@gmail.com>
2022-11-01 20:53:03 +0900

    feat: 답변 작성 기능 구현

--- src/components/QuestionDetail/AnswerEditor.js
+++ src/components/QuestionDetail/AnswerEditor.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
 import React, { useState } from 'react';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 function AnswerEditor() {
+	const queryClient = useQueryClient();
 	const [quillText, setQuillText] = useState('');
 	const handleTextChange = (e) => {
 		setQuillText(e);
@@ -19,17 +20,24 @@ function AnswerEditor() {
 		);
 	});
 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const newAnser = {
+			questionContent: quillText,
+		};
+		addAnswer.mutate(newAnser, {
+			onSuccess: () => {
+				return queryClient.invalidateQueries(['question', questionId]);
+			},
+		});
+	};
+
 	return (
 		<main>
 			<article>
 				<section className="h-[500px] w-[770px] mt-4">
 					<h3 className="text-xl mb-4">Your Answer</h3>
-					<div dangerouslySetInnerHTML={{ __html: quillText }} />
-					<form
-						onSubmit={(event) => {
-							event.preventDefault();
-						}}
-					>
+					<div>
 						<ReactQuill
 							theme="snow"
 							value={quillText}
@@ -39,10 +47,11 @@ function AnswerEditor() {
 						<button
 							className="mt-16 rounded-sm text-sm p-2 text-white bg-[#0a94ff] hover:bg-[#0074CC]"
 							type="submit"
+							onClick={handleSubmit}
 						>
 							Ask Question
 						</button>
-					</form>
+					</div>
 				</section>
 			</article>
 		</main>

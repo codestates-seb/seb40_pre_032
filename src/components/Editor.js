6a2fbe0f5073a18a2008966c6566951030459485
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-02 09:14:44 +0900

    react-hook-form 부분 삭제,useMutation 적용

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,52 +1,44 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ReactQuill from 'react-quill';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import 'react-quill/dist/quill.snow.css';
-import { useForm } from 'react-hook-form';
 import TagInput from './TagInput';
 import Accordian from './Accordian';
+import axios from 'axios';
 
 function Editor() {
+	const queryClient = useQueryClient();
 	const navigate = useNavigate();
-	const [isPending, setIsPending] = useState(false);
-	const {
-		register,
-		handleSubmit,
-		setValue,
-		watch,
-		formState: { errors },
-	} = useForm({ mode: 'onChange' });
 
-	useEffect(() => {
-		register('question_content', { required: true, minLength: 11 });
-	}, []);
+	const [title, setTitle] = useState('');
+	const [quillText, setQuillText] = useState('');
+	const [tags, setTags] = useState([]);
 
-	const onEditorStateChange = (editorState) => {
-		setValue('question_content', editorState);
+	const handleTitleChange = (event) => setTitle(event.target.value);
+
+	const handleTextChange = (event) => {
+		setQuillText(event);
 	};
 
-	const postData = (data) => {
-		fetch('http://localhost:4000/data', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(data),
-		}).then(() => {
-			setIsPending(false);
-			navigate('/');
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const newQuestion = { title, questionContent: quillText, tags };
+		addQuestion.mutate(newQuestion, {
+			onSuccess: () => {
+				navigate('/');
+				return queryClient.invalidateQueries(['questions']);
+			},
 		});
 	};
-	const { mutate } = useMutation(postData);
 
-	const onSubmit = () => {
-		setIsPending(true);
-		mutate(postData);
-	};
-
-	const editorContent = watch('question_content');
+	const addQuestion = useMutation((newQuestion) =>
+		axios.post('http://localhost:3001/data', newQuestion),
+	);
 
 	return (
-		<main className="flex items-center mx-auto">
+		<main className="flex items-center mx-auto justify-center">
 			<article>
 				<h1 className="font-bold ml-12 mt-4 text-2xl">Ask a Question</h1>
 				<section className=" shadow w-[1000px] h-[800px] px-2 mx-10 mt-10 border border-gray-600 bg-white">
@@ -55,52 +47,35 @@ function Editor() {
 						Be specific and imagine you’re asking a question to another person
 					</div>
 					<input
-						/* eslint-disable react/jsx-props-no-spreading */
-						{...register('title', { required: true })}
+						value={title}
+						onChange={handleTitleChange}
 						type="text"
 						className="w-full border-2 px-2 py-1 leading-loose"
 						placeholder="e.g Is there an R function for findiong the index of an element in vector?"
 					/>
-					{errors.title?.message}
+
 					<h3 className="font-semibold mt-2">Body</h3>
 					<span className="text-gray-700">
 						Include all the information someone would need to answer your
 						question
 					</span>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div dangerouslySetInnerHTML={{ __html: editorContent }} />
-						<ReactQuill
-							{...register('question_content', { required: true })}
-							theme="snow"
-							value={editorContent}
-							onChange={onEditorStateChange}
-							className="h-96 px-2 mt-3 relative right-2 shadow-gray-700"
-						/>
-						<p className="px-10">
-							{errors.question_content?.message && '입력해주세요'}
-						</p>
-						<TagInput />
-						{!isPending ? (
-							<button
-								className="text-white h-10 mt-10 ml-3 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
-								type="submit"
-								value="Review Your Question"
-								onClick={handleSubmit(onSubmit)}
-							>
-								Review Your Question
-							</button>
-						) : (
-							<button
-								className="text-white h-10 mt-10 ml-3 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
-								type="submit"
-								value="Review Your Question"
-								onClick={handleSubmit(onSubmit)}
-								disabled
-							>
-								Review Your Question
-							</button>
-						)}
-					</form>
+
+					<div dangerouslySetInnerHTML={{ __html: quillText }} />
+					<ReactQuill
+						theme="snow"
+						value={quillText}
+						onChange={handleTextChange}
+						className="h-96 px-2 mt-3 relative right-2 shadow-gray-700"
+					/>
+					<p className="px-10">{/*  */}</p>
+					<TagInput tags={tags} setTags={setTags} />
+					<button
+						className="text-white h-10 mt-10 ml-3 items-center cursor-pointer w-1/6 flex justify-center bg-blue-400 mx-auto hover:bg-blue-500"
+						type="submit"
+						onClick={handleSubmit}
+					>
+						Add Question
+					</button>
 				</section>
 			</article>
 			<Accordian />

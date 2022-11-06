fd18e6830bdd5482e54b3a641acb4c33bc2aad3b
dohyeons <ddhhss0603@gmail.com>
2022-11-06 18:14:23 +0900

    feat: 이름 클릭 시 작성게시물 렌더링 기능 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		screens: {
+			sm: { max: '767px' },
+			// => @media (min-width: 640px and max-width: 767px) { ... }
+
+			md: { min: '768px', max: '1023px' },
+			// => @media (min-width: 768px and max-width: 1023px) { ... }
+
+			lg: { min: '1024px' },
+			// => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+			// xl: { min: '1280px', max: '1535px' },
+			// // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+			// '2xl': { min: '1536px' },
+			// // => @media (min-width: 1536px) { ... }
+		},
 		extend: {
 			colors: {
 				'border-blue': 'hsl(205,41%,63%)',
